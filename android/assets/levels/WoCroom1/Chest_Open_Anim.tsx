d8e325f2ab467a628b381f8c07899851cb489a91
Thorasta1 <78131963+Thorasta1@users.noreply.github.com>
2021-01-30 14:57:50 -0800

    Chest added

---
+++ android/assets/levels/WoCroom1/Chest_Open_Anim.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Chest_Open_Anim" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="../../images/prepacked/Chest_Open_Anim.png" width="96" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="1000"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="1000"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
