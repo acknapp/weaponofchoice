fbed74b6e9f9d1c6fb7fa993b56c8b48d58bdb21
Andrew Knapp <acknapp@gmail.com>
2021-01-29 21:18:05 -0800

    updated music file loaded to see if quality was different for a 128Kbps mp3 file

--- core/src/com/weaponofchoice/game/util/Constants.java
+++ core/src/com/weaponofchoice/game/util/Constants.java
@@ -1,7 +1,6 @@
 package com.weaponofchoice.game.util;
 
 public class Constants {
-    public static final String LEVEL_MUSIC_NAME = "GGJAM - CHILL 1"; // Change this to the song name
+    public static final String LEVEL_MUSIC_NAME = "GGJAM - CHILL_EPIC THEME IDEA 2 1"; // Change this to the song name
     public static final String MUSIC = "music/" + LEVEL_MUSIC_NAME + ".mp3";
-
 }
