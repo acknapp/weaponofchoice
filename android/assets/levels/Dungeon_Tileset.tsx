336fbbba090498211cc435ce8a0aa2131eb3f178
Thorasta1 <78131963+Thorasta1@users.noreply.github.com>
2021-01-29 20:17:17 -0800

    Making demo room.

--- android/assets/levels/Dungeon_Tileset.tsx
+++ android/assets/levels/Dungeon_Tileset.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Dungeon_Tileset" tilewidth="32" tileheight="32" tilecount="320" columns="16">
  <image source="../images/Dungeon_Tileset.png" width="512" height="640"/>
+ <tile id="246">
+  <animation>
+   <frame tileid="245" duration="100"/>
+   <frame tileid="244" duration="100"/>
+   <frame tileid="243" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
