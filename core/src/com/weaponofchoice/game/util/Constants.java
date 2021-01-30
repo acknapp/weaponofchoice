dcd727ca6b8b21e379aea80de3b49b39b2ba9b7d
Thorasta1 <78131963+Thorasta1@users.noreply.github.com>
2021-01-30 15:41:21 -0800

    stuff

--- core/src/com/weaponofchoice/game/util/Constants.java
+++ core/src/com/weaponofchoice/game/util/Constants.java
@@ -2,8 +2,8 @@ package com.weaponofchoice.game.util;
 
 public class Constants {
     // Levels
-    public static final String LEVEL_MAP_NAME = "WoCFirstFry";
-    public static final String LEVEL_MAP = "levels/" + LEVEL_MAP_NAME + ".tmx";
+    public static final String LEVEL_MAP_NAME = "WoCroom1";
+    public static final String LEVEL_MAP = "levels/WoCroom1/" + LEVEL_MAP_NAME + ".tmx";
 
     // Music
     public static final String LEVEL_MUSIC_NAME = "GGJAM - DRONE THEME"; // Change this to the song name
