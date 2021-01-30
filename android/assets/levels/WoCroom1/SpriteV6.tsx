2b773449e5f17e6c92739a01d0b7b0e96d494992
Thorasta1 <78131963+Thorasta1@users.noreply.github.com>
2021-01-30 14:18:43 -0800

    Moving doors

---
+++ android/assets/levels/WoCroom1/SpriteV6.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="SpriteV6" tilewidth="32" tileheight="32" tilecount="320" columns="16">
+ <image source="../../images/prepacked/SpriteV6.png" width="512" height="640"/>
+ <tile id="128">
+  <animation>
+   <frame tileid="129" duration="2000"/>
+   <frame tileid="130" duration="100"/>
+   <frame tileid="131" duration="100"/>
+   <frame tileid="132" duration="100"/>
+   <frame tileid="133" duration="2000"/>
+   <frame tileid="132" duration="100"/>
+   <frame tileid="131" duration="100"/>
+   <frame tileid="130" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
