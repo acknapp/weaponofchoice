c4fcd224a885ded20d4ccabeb7dae50394ca77f0
Andrew Knapp <acknapp@gmail.com>
2021-01-30 11:10:28 -0800

    changin out music file

--- core/src/com/weaponofchoice/game/util/Constants.java
+++ core/src/com/weaponofchoice/game/util/Constants.java
@@ -6,6 +6,6 @@ public class Constants {
     public static final String LEVEL_MAP = "levels/" + LEVEL_MAP_NAME + ".tmx";
 
     // Music
-    public static final String LEVEL_MUSIC_NAME = "GGJAM - CHILL_EPIC THEME IDEA 2 1"; // Change this to the song name
+    public static final String LEVEL_MUSIC_NAME = "GGJAM - DRONE THEME"; // Change this to the song name
     public static final String MUSIC = "music/" + LEVEL_MUSIC_NAME + ".mp3";
 }
