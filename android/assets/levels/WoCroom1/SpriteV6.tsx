53c6b5fbd0358805bf326a08037188de8d46e156
Thorasta1 <78131963+Thorasta1@users.noreply.github.com>
2021-01-31 10:23:56 -0800

    maybe added collision logic

--- android/assets/levels/WoCroom1/SpriteV6.tsx
+++ android/assets/levels/WoCroom1/SpriteV6.tsx
@@ -1,6 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="SpriteV6" tilewidth="32" tileheight="32" tilecount="320" columns="16">
  <image source="../../images/prepacked/SpriteV6.png" width="512" height="640"/>
+ <tile id="33">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="128">
   <animation>
    <frame tileid="129" duration="2000"/>
