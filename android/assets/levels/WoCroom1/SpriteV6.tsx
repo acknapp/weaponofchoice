f8a14f33d7f37d3a9415faba4de00539377c2d1e
Thorasta1 <78131963+Thorasta1@users.noreply.github.com>
2021-01-31 15:31:13 -0800

    animating lava

--- android/assets/levels/WoCroom1/SpriteV6.tsx
+++ android/assets/levels/WoCroom1/SpriteV6.tsx
@@ -1,11 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="SpriteV6" tilewidth="32" tileheight="32" tilecount="320" columns="16">
  <image source="../../images/prepacked/SpriteV6.png" width="512" height="640"/>
- <tile id="33">
-  <objectgroup draworder="index" id="2">
-   <object id="1" name="collide" x="2.10526" y="2.23684" width="27.3947" height="27.1316"/>
-  </objectgroup>
- </tile>
  <tile id="128">
   <animation>
    <frame tileid="129" duration="2000"/>
@@ -24,10 +19,66 @@
    <frame tileid="130" duration="100"/>
    <frame tileid="131" duration="100"/>
    <frame tileid="132" duration="100"/>
-   <frame tileid="133" duration="1000"/>
+   <frame tileid="133" duration="100"/>
    <frame tileid="132" duration="100"/>
    <frame tileid="131" duration="100"/>
    <frame tileid="130" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="151">
+  <animation>
+   <frame tileid="151" duration="600"/>
+   <frame tileid="152" duration="600"/>
+   <frame tileid="168" duration="600"/>
+   <frame tileid="167" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="152">
+  <animation>
+   <frame tileid="152" duration="650"/>
+   <frame tileid="168" duration="650"/>
+   <frame tileid="167" duration="650"/>
+   <frame tileid="151" duration="650"/>
+  </animation>
+ </tile>
+ <tile id="153">
+  <animation>
+   <frame tileid="153" duration="300"/>
+   <frame tileid="154" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="154">
+  <animation>
+   <frame tileid="154" duration="300"/>
+   <frame tileid="153" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="167">
+  <animation>
+   <frame tileid="167" duration="750"/>
+   <frame tileid="151" duration="750"/>
+   <frame tileid="152" duration="750"/>
+   <frame tileid="168" duration="750"/>
+  </animation>
+ </tile>
+ <tile id="168">
+  <animation>
+   <frame tileid="168" duration="700"/>
+   <frame tileid="167" duration="700"/>
+   <frame tileid="151" duration="700"/>
+   <frame tileid="152" duration="700"/>
+  </animation>
+ </tile>
+ <tile id="169">
+  <animation>
+   <frame tileid="169" duration="300"/>
+   <frame tileid="170" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="170">
+  <animation>
+   <frame tileid="170" duration="300"/>
+   <frame tileid="169" duration="300"/>
   </animation>
  </tile>
 </tileset>
